6baae451bb9f7754fdec5f078e9f9f0920620f5e
Antonis Papaioannou <mrantonp@gmail.com>
2024-03-13 23:07:49 +0200

    complete billboard on container

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,18 @@
-const HomePage = () => {
+
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("f7ef266f-cf35-4692-9967-edc4302c8c5e");
   return ( 
-    <div>
-      Store
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
+      </div>
+    </Container>
    );
 }
  
