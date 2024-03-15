5f35876d4c02fbe95c05f01aa7a6f69d2579ab19
Antonis Papaioannou <mrantonp@gmail.com>
2024-03-15 19:33:20 +0200

    complete preview image

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,29 +1,38 @@
-"use client";
+'use client';
 
-import { Product } from "@/types";
-import Image from "next/image";
-import IconButton from "./icon-button";
-import { Expand, ShoppingCart } from "lucide-react";
-import Currency from "./currency";
-import { useRouter } from "next/navigation";
+import { Product } from '@/types';
+import Image from 'next/image';
+import IconButton from './icon-button';
+import { Expand, ShoppingCart } from 'lucide-react';
+import Currency from './currency';
+import { useRouter } from 'next/navigation';
+import { MouseEventHandler } from 'react';
+import usePreviewModal from '@/hooks/use-preview-modal';
 
 interface ProductCard {
   data: Product;
 }
 
-const ProductCard: React.FC<ProductCard> = ({
-  data
-}) => {
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
 
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
 
-  return ( 
-    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    previewModal.onOpen(data);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
-        <Image 
+        <Image
           alt="Image"
           src={data?.images?.[0]?.url}
           fill
@@ -31,30 +40,26 @@ const ProductCard: React.FC<ProductCard> = ({
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton 
-              onClick={() => {}}
-              icon={<Expand size={20} className="text-gray-600"/>}
+            <IconButton
+              onClick={onPreview}
+              icon={<Expand size={20} className="text-gray-600" />}
             />
-            <IconButton 
+            <IconButton
               onClick={() => {}}
-              icon={<ShoppingCart size={20} className="text-gray-600"/>}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
         </div>
       </div>
       <div className="">
-        <p className="font-semibold text-lg">
-          {data.name}
-        </p>
-        <p className="text-sm text-gray-500">
-          {data.category?.name}
-        </p>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
       </div>
       <div className="flex items-center justify-between">
-        <Currency value={data?.price}/>
+        <Currency value={data?.price} />
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default ProductCard;
